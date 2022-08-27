05572dfe095617726580d6ebad4278a44fe8fb07
David Vardanyan <dave.indevops@gmail.com>
2022-08-27 17:56:32 +0400

    SEcurity group and ingress and egress rules

--- main.tf
+++ main.tf
@@ -3,37 +3,39 @@ provider "aws" {
 }
 
 variable "vpc_cidr_block" {
-  
+
 }
 
 variable "subnet_cidr_block" {
-  
+
 }
 
 variable "availability_zone" {
-  
+
 }
 
 variable "env_prefix" {
-    
+
 }
+
+
 
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
-    Name: "${var.env_prefix}-vpc"
-    
+    Name : "${var.env_prefix}-vpc"
+
   }
 }
 
 
 resource "aws_subnet" "myapp-subnet" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  cidr_block = var.subnet_cidr_block
+  vpc_id            = aws_vpc.myapp-vpc.id
+  cidr_block        = var.subnet_cidr_block
   availability_zone = var.availability_zone
   tags = {
-    Name: "${var.env_prefix}-subnet"
+    Name : "${var.env_prefix}-subnet"
   }
 }
 
@@ -41,18 +43,18 @@ resource "aws_subnet" "myapp-subnet" {
 resource "aws_internet_gateway" "myapp-igw" {
   vpc_id = aws_vpc.myapp-vpc.id
 
-   tags = {
+  tags = {
     Name = "${var.env_prefix}-igw"
 
- }
+  }
 }
 
 resource "aws_route_table" "myapp-route-table" {
   vpc_id = aws_vpc.myapp-vpc.id
 
   route {
-     cidr_block = "0.0.0.0/0"
-     gateway_id = aws_internet_gateway.myapp-igw.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
   }
 
   tags = {
@@ -62,8 +64,40 @@ resource "aws_route_table" "myapp-route-table" {
 }
 
 resource "aws_route_table_association" "assoc-rtb-subnet" {
-  subnet_id = aws_subnet.myapp-subnet.id
+  subnet_id      = aws_subnet.myapp-subnet.id
   route_table_id = aws_route_table.myapp-route-table.id
+}
+
+
+resource "aws_security_group" "myapp-sg" {
+  name   = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["78.109.71.102/32"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${var.env_prefix}-sg"
+
+  }
 }
 
 
