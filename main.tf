d323714dcbef734dfc82041e6a214fee535662b4
David Vardanyan <dave.indevops@gmail.com>
2022-08-27 15:29:30 +0400

    Added variables and tags for vpc and subnet

--- main.tf
+++ main.tf
@@ -3,40 +3,42 @@ provider "aws" {
 }
 
 variable "vpc_cidr_block" {
-    description = "vpc cidr block"
   
-}
-
-resource "aws_vpc" "dev-vpc" {
-  cidr_block = var.vpc_cidr_block
-  tags = {
-    Name: "development"
-    vpc_env: "dev"
-  }
 }
 
 variable "subnet_cidr_block" {
-  description = "subnet cidr block"
   
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-  vpc_id = aws_vpc.dev-vpc.id
-  cidr_block = var.subnet_cidr_block
-  availability_zone = "us-east-1a"
+variable "availability_zone" {
+  
+}
+
+variable "env_prefix" {
+    
+}
+
+
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
   tags = {
-    Name: "dev-subnet-1"
+    Name: "${var.env_prefix}-vpc"
+    
   }
 }
 
 
-output "dev-vpc-id" {
-    value = aws_vpc.dev-vpc
+resource "aws_subnet" "myapp-subnet" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.availability_zone
+  tags = {
+    Name: "${var.env_prefix}"
+  }
 }
 
-output "dev-subnet-id" {
-    value =  aws_subnet.dev-subnet-1
-}
+
+
 
 
 
